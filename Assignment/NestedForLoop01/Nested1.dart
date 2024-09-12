447a2fa91b438ab00edf6abc620003e909def3ec
vaibhav01patil <vaibhav003patil@gmail.com>
2024-09-12 16:36:34 +0530

    new file:   Assignment/NestedForLoop01/Nested1.dart 	new file:   Assignment/NestedForLoop01/Nested2.dart 	new file:   Assignment/NestedForLoop01/Nested3.dart 	new file:   Assignment/NestedForLoop01/Nested4.dart 	new file:   Assignment/NestedForLoop01/Nested5.dart

---
+++ Assignment/NestedForLoop01/Nested1.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  int row = 4;
+  for (int i = 1; i <= row; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write(" C2W ");
+    }
+    print(" ");
+  }
+}
---
+++ Assignment/NestedForLoop01/Nested3.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  int row = 4;
+  int num = 10;
+  for (int i = 1; i <= row; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write(" $num ");
+      num--;
+    }
+    print(" ");
+  }
+}
